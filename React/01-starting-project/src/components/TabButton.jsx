89d5a3d03954d13aae653d32e46fc51b8892121b
naru <lyn.style@gmail.com>
2025-12-22 07:56:20 +0900

    update

--- React/01-starting-project/src/components/TabButton.jsx
+++ React/01-starting-project/src/components/TabButton.jsx
@@ -1,12 +1,12 @@
 // props.childres은 컴포넌트 텍스트 사이 내용을 의미함..
 //  <TabButton>Components</TabButton> <--- 여기의 "Components" 부분
 
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   console.log("TAB BUTTON COMPONENT EXECUTING");
 
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
     </li>
   );
 }
