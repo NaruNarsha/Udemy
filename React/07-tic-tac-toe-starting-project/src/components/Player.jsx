18e53efd0abe905beace86d55b7a6ccc7307abf5
naru <lyn.style@gmail.com>
2026-01-25 14:45:04 +0900

    77. 옛 State(상태)를 기반으로 올바르게 상태 업데이트하기

--- React/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ React/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,14 +3,22 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
-  function handleEditClick() {
-    setIsEditing(true);
-  }
+  const handleEditClick = () => {
+    console.log("Edit button clicked -  call handleEditClick()");
+        
+    // 새 상태가 이전 상태 값에 의존하는 경우 다음과 같이 상태를 업데이트해서는 안 됩니다
+    // setIsEditing(!isEditing);
+
+    // 대신 상태 업데이트 함수에 함수를 전달하면 이 함수는 React에 의해 자동으로 호출되며 보장된 최신 상태 값을 받게 됩니다
+    setIsEditing((editing) => !editing);
+  };
 
   let playerName = <span className="player-name">{name}</span>;
+  //let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value ={name}/>;
+    //btnCaption = "Save";
   }
 
   return (
@@ -19,7 +27,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
