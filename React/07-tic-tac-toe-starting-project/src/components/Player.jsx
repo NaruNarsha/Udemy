cd9f0e17dd97e58a94f3ea2e5b53eb038c1a87c3
문경림 <naru@takeone.co.kr>
2026-01-26 19:59:18 +0900

    78. 사용자 입력 & 양방향 바인딩

--- React/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ React/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
     console.log("Edit button clicked -  call handleEditClick()");
-        
+
     // 새 상태가 이전 상태 값에 의존하는 경우 다음과 같이 상태를 업데이트해서는 안 됩니다
     // setIsEditing(!isEditing);
 
@@ -13,18 +14,33 @@ export default function Player({ name, symbol }) {
     setIsEditing((editing) => !editing);
   };
 
-  let playerName = <span className="player-name">{name}</span>;
+  const handlePlayerName = (event) => {
+    console.log(
+      "Player name changed - call handlePlayerName()",
+      event.target.value,
+    );
+    setPlayerName(event.target.value);
+  };
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   //let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" required value ={name}/>;
-    //btnCaption = "Save";
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handlePlayerName}
+      />
+    );
+    // btnCaption = "Save";
   }
 
   return (
     <li>
       <span className="Player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
