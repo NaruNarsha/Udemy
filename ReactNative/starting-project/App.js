8d06875d3a4d57e74f51410e0ee148af6eca5564
naru <lyn.style@gmail.com>
2026-01-31 14:39:24 +0900

    RN 15. 핵심 컴포넌트로 작업하기

--- ReactNative/starting-project/App.js
+++ ReactNative/starting-project/App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+// <View> <View/> 안에는 복수의 <Text> 컴포넌트를 넣을 수 있습니다.
+// View :: 컴포넌트를 담는 데 사용하는 유일한 컨테이너 컴포넌트
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
+      <Text>Hello World!</Text>
+      <Button title='Tab me!' /> 
     </View>
   );
 }
