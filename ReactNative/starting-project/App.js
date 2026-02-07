b189692fe7ef4a443abce043b140b94836da07e0
naru <lyn.style@gmail.com>
2026-02-07 16:44:15 +0900

    RN 22. 레이아웃 개선하기, 23 이벤트 처리하기

--- ReactNative/starting-project/App.js
+++ ReactNative/starting-project/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 // React Native의 모든 컴포넌트는 반드시 하나의 최상위 컴포넌트로 감싸져야 합니다.
@@ -11,47 +12,84 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 //    - Button 컴포넌트는 title 속성을 사용하여 버튼에 표시될 텍스트를 지정합니다.
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+
+
+  // 사용자가 내용을 입력할 때 해당 입력값을 가져노는 역할
+  function goalInputHandler(enteredText) {
+    console.log(enteredText);
+
+    setEnteredGoalText(enteredText);
+  };
+
+  // 버튼을 누르면 작동하는 함수
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  };
+
   // styles는 View나 Text 컴포넌트에 스타일을 적용하는 데 사용됩니다.
+  // onChangeText :: React Naitve에서 활용하는 프로퍼티로 텍스트 입력이 변경될 때마다 호출되는 콜백 함수를 지정합니다.
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder = 'Your course goal!'/>
-        <Button title = 'Add Goal' />
+        <TextInput 
+          style={styles.textInput} 
+          placeholder = 'Your course goal!' 
+          onChangeText = {goalInputHandler}
+        />
+        <Button title = 'Add Goal' onPress ={addGoalHandler}  />
       </View>
-      <View>
+      <View style = {styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
   );
 }
 
+
+
+//========================================================================================================
 // padding :: 요소 안쪽 여백
 
 // stylesheet 생성
-// - StyleSheet.create 메서드를 사용하여 스타일 객체를 생성합니다.
-// - 스타일 객체는 CSS와 유사한 속성-값 쌍으로 구성됩니다.
-// - 스타일 객체는 컴포넌트의 style 속성에 전달되어 적용됩니다.
-// - 스타일 속성은 카멜 케이스(camelCase) 표기법을 사용합니다.
-//  - 예: background-color -> backgroundColor
-// - 스타일 속성 값은 문자열 또는 숫자로 지정할 수 있습니다.
-// - 스타일 객체는 재사용이 가능하며, 여러 컴포넌트에 동일한 스타일을 적용할 수 있습니다.
-// - StyleSheet.create 메서드를 사용하면 성능 최적화가 가능합니다.
-// - 스타일 객체는 불변(immutable)으로 생성되므로, 런타임에 변경할 수 없습니다.
+//    - StyleSheet.create 메서드를 사용하여 스타일 객체를 생성합니다.
+//    - 스타일 객체는 CSS와 유사한 속성-값 쌍으로 구성됩니다.
+//    - 스타일 객체는 컴포넌트의 style 속성에 전달되어 적용됩니다.
+//    - 스타일 속성은 카멜 케이스(camelCase) 표기법을 사용합니다.
+//     - 예: background-color -> backgroundColor
+//    - 스타일 속성 값은 문자열 또는 숫자로 지정할 수 있습니다.
+//    - 스타일 객체는 재사용이 가능하며, 여러 컴포넌트에 동일한 스타일을 적용할 수 있습니다.
+//    - StyleSheet.create 메서드를 사용하면 성능 최적화가 가능합니다.
+//    - 스타일 객체는 불변(immutable)으로 생성되므로, 런타임에 변경할 수 없습니다.
+//========================================================================================================
 const styles = StyleSheet.create({
   appContainer : {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    backgroundColor: '#d1b5b5',
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+    //backgroundColor: '#e4bebe',
   },
   textInput : {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8
-  }
+  },
+  goalsContainer: {
+    flex: 4,
+    //backgroundColor: 'yellow',
+  }   
 
 });
 
