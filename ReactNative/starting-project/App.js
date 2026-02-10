086abe7036be1cd4a94e2fd89e506643ddd463f0
문경림 <naru@takeone.co.kr>
2026-02-10 20:44:12 +0900

    RN 30. "Goal Input" 컴포넌트로 작업하기

--- ReactNative/starting-project/App.js
+++ ReactNative/starting-project/App.js
@@ -1,13 +1,7 @@
 import { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  Button,
-  TextInput,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import { StyleSheet, View, ScrollView, FlatList } from "react-native";
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 // React Native의 모든 컴포넌트는 반드시 하나의 최상위 컴포넌트로 감싸져야 합니다.
 //  - <View> <View/> 안에는 복수의 <Text> 컴포넌트를 넣을 수 있습니다.
@@ -20,18 +14,10 @@ import GoalItem from "./components/GoalItem";
 //    - Button 컴포넌트는 title 속성을 사용하여 버튼에 표시될 텍스트를 지정합니다.
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  // 사용자가 내용을 입력할 때 해당 입력값을 가져노는 역할
-  function goalInputHandler(enteredText) {
-    //console.log(enteredText);
-
-    setEnteredGoalText(enteredText);
-  }
-
   // 버튼을 누르면 작동하는 함수
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     //console.log(enteredGoalText);
 
     // 기존의 courseGoals 배열을 복사하고, 새로운 목표를 추가
@@ -52,14 +38,7 @@ export default function App() {
   // onChangeText :: React Naitve에서 활용하는 프로퍼티로 텍스트 입력이 변경될 때마다 호출되는 콜백 함수를 지정합니다.
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your course goal!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
           data={courseGoals}
@@ -103,23 +82,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     backgroundColor: "#d1b5b5",
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-    //backgroundColor: '#e4bebe',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
+
   goalsContainer: {
     flex: 5,
     //backgroundColor: 'yellow',
---
+++ ReactNative/starting-project/components/GoalInput.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
+
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  // 사용자가 내용을 입력할 때 해당 입력값을 가져노는 역할
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText(""); // 새로운 목표가 추가된 후 입력 필드를 초기화
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Your course goal!"
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add Goal" onPress={addGoalHandler} />
+    </View>
+  );
+}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    //backgroundColor: '#e4bebe',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
