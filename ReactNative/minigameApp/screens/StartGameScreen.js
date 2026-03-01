4c5c9545e7a50af054c1a2132b9a05a015f2b320
naru <lyn.style@gmail.com>
2026-03-01 14:57:26 +0900

    RN 51. "Number Input" 요소 스타일링하기

--- ReactNative/minigameApp/screens/StartGameScreen.js
+++ ReactNative/minigameApp/screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style = {styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2}/>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -20,12 +20,25 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    borderRadius: 8,
     backgroundColor: '#72063c',
+    borderRadius: 8,
     elevation: 4,       // only for android, for ios we need to use shadow properties
     shadowColor: 'black', // only for ios
     shadowOffset: { width: 0, height: 2 }, // only for ios
     shadowRadius: 6, // only for ios
     shadowOpacity: 0.25, // only for ios
+  },
+  numberInput: {
+    height: 60,
+    width: 50,
+    fontSize: 32,
+    //backgroundColor: '#e4d0ff', // 영역 체크용 background color
+    //borderRadius: 8,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
 });
