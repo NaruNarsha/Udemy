caf44dc35c314f3040c5a4e6513727ce852a48b2
naru <lyn.style@gmail.com>
2026-03-01 14:36:20 +0900

    RN 50. Android & iOS용 스타일링하기

--- ReactNative/minigameApp/screens/StartGameScreen.js
+++ ReactNative/minigameApp/screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import {TextInput, View} from 'react-native';
+import {TextInput, View, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style = {styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,20 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+
+const styles = StyleSheet.create({
+
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: '#72063c',
+    elevation: 4,       // only for android, for ios we need to use shadow properties
+    shadowColor: 'black', // only for ios
+    shadowOffset: { width: 0, height: 2 }, // only for ios
+    shadowRadius: 6, // only for ios
+    shadowOpacity: 0.25, // only for ios
+  }
+});
