e5102af28b7d65c17f061c8bd9a58dc48f42df25
naru <lyn.style@gmail.com>
2026-03-03 21:32:04 +0900

    RN 57. 배경 이미지 추가하기

--- ReactNative/minigameApp/App.js
+++ ReactNative/minigameApp/App.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, ImageBackground} from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -7,7 +7,14 @@ import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground 
+        source={require('./assets/images/background.png')} 
+        resizeMode='cover'
+        style = {styles.rootScreen}
+        imageStyle = {styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -24,5 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1, // 전체 화면을 차지하도록 설정
+  },
+  backgroundImage: {
+    opacity: 0.15, // 배경 이미지의 투명도 설정
   }
 });
